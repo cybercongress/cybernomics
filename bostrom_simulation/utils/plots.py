ca2c559ae83954b6bc7f428c57bf897fcbd23e76
Snedashkovsky <snedashkovsky@gmail.com>
2021-10-01 16:26:18 +0800

    Add data type checks for plots

--- bostrom_simulation/utils/plots.py
+++ bostrom_simulation/utils/plots.py
@@ -1,19 +1,20 @@
 import plotly.express as px
 import seaborn as sns
+import pandas as pd
 
 
-def df_preparator(df):
+def df_preparator(df: pd.DataFrame) -> pd.DataFrame:
     # df = df.drop(df[df['timestep']%10 != 0].index)
     return df
 
 
 year = dict(
-        tick0 = 0,
-        dtick = 365
+        tick0=0,
+        dtick=365
     )
 
 
-def linear_plot(df, _y, render='sns', figsize: tuple=(11.7, 8.27)):
+def linear_plot(df: pd.DataFrame, _y: str, render: str = 'sns', figsize: tuple = (11.7, 8.27)) -> None:
     if render == 'px':
         fig = px.line(
             df,
@@ -30,7 +31,7 @@ def linear_plot(df, _y, render='sns', figsize: tuple=(11.7, 8.27)):
         sns.lineplot(data=df, x='timestep', y=_y)
 
 
-def scatter_plot(df, _y, render='sns'):
+def scatter_plot(df: pd.DataFrame, _y: str, render: str = 'sns') -> None:
     if render == 'px':
         fig = px.scatter(
             df,
@@ -43,13 +44,14 @@ def scatter_plot(df, _y, render='sns'):
             labels={'color': _y}
         )
         fig.update_layout(
-        margin=dict(l=20, r=20, t=20, b=20), xaxis=year)
+            margin=dict(l=20, r=20, t=20, b=20), xaxis=year)
         fig.show()
     elif render == 'sns':
         sns.lmplot('timestep', _y, data=df, fit_reg=True)
 
 
-def plot_line_2_diff_y(df, value_1, value_2, figsize: tuple = (16,9), value_1_log=False, value_2_log=False):
+def plot_line_2_diff_y(df: pd.DataFrame, value_1: str, value_2: str, figsize: tuple = (16, 9), value_1_log: bool = False,
+                       value_2_log: bool = False) -> None:
     value_1_df = df[[value_1]]
     value_2_df = df[[value_2]]
     ax1 = value_1_df.plot.line(figsize=figsize, logy=value_1_log, xticks=range(0, 3650, 365), grid=True)
@@ -58,5 +60,6 @@ def plot_line_2_diff_y(df, value_1, value_2, figsize: tuple = (16,9), value_1_lo
     value_2_df.plot.line(ax=ax2, figsize=(16, 9), xticks=range(0, 3650, 365), style={value_2: 'r'}, logy=value_2_log, grid=True)
 
 
-def plot_line_2_same_y(df, values:list, figsize: tuple = (16,9), value_1_log=False, value_2_log=False):
+def plot_line_2_same_y(df: pd.DataFrame, values: list, figsize: tuple = (16, 9), value_1_log: bool = False,
+                       value_2_log: bool = False) -> None:
     df.plot(y=values, figsize=(16, 9), xticks=range(0, 3650, 365), grid=True)
