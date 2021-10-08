46e3ceb1da756853f6967298d3f6eaec2ae66a3b
Snedashkovsky <snedashkovsky@gmail.com>
2021-10-08 12:30:32 +0800

    Update the plots

--- bostrom_simulation/utils/plots.py
+++ bostrom_simulation/utils/plots.py
@@ -1,133 +1,127 @@
-# import plotly.express as px
-# import seaborn as sns
-# import pandas as pd
+import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-path = './images/'
-_figsize = (16, 7)
-_xticks = range(0, 4015, 365)
-_xlabel = "timestep(days)"
+IMAGES_PATH = './images/'
+FIGSIZE = (16, 7)
+XTICKS = range(0, 4015, 365)
+XLABEL = "timestep(days)"
 
 
-def boot_supply_plot(df, title='boot supply and inflation rate', figsize=_figsize):
+def boot_supply_plot(df: pd.DataFrame, title: str = 'BOOT Supply and Inflation Rate', figsize: tuple = FIGSIZE):
     columns = ['boot_liquid_supply', 'boot_frozen_supply', 'boot_bonded_supply']
-    ax1 = df.plot.area(y=columns, linewidth=0, colormap="winter", xticks=_xticks, grid=True, figsize=figsize)
-    ax1.set(xlabel=_xlabel, ylabel="boots")
+    ax1 = df.plot.area(y=columns, linewidth=0, colormap="winter", xticks=XTICKS, grid=True, figsize=figsize)
+    ax1.set(xlabel=XLABEL, ylabel="BOOT")
     ax1.set_title(title, size=16, fontweight="bold")
     ax2 = ax1.twinx()
     ax2.spines['right'].set_position(('axes', 1.0))
     ax2.set(ylabel="rate")
-    df.plot.line(ax=ax2, y='boot_inflation_rate', figsize=figsize, xticks=_xticks, grid=True)
+    df.plot.line(ax=ax2, y='boot_inflation_rate', figsize=figsize, xticks=XTICKS, grid=True)
     plt.legend(bbox_to_anchor=(1.0, 1.0))
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def hydrogen_supply_plot(df, title='hydrogen supply', figsize=_figsize):
-    ax = df.plot.line(y='hydrogen_supply', figsize=figsize, xticks=_xticks, grid=True)
-    ax.set(xlabel=_xlabel, ylabel="hydrogens")
+def hydrogen_supply_plot(df: pd.DataFrame, title: str = 'HYDROGEN Supply', figsize: tuple = FIGSIZE):
+    ax = df.plot.line(y='hydrogen_supply', figsize=figsize, xticks=XTICKS, grid=True)
+    ax.set(xlabel=XLABEL, ylabel="HYDROGEN")
     ax.set_title(title, size=16, fontweight="bold")
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def agents_count_plot(df, title='agents count', figsize=_figsize):
-    ax = df.plot.line(y='agents_count', figsize=figsize, xticks=_xticks, grid=True)
-    ax.set(xlabel=_xlabel, ylabel="count")
+def agents_count_plot(df: pd.DataFrame, title: str = 'Agents Count', figsize: tuple = FIGSIZE):
+    ax = df.plot.line(y='agents_count', figsize=figsize, xticks=XTICKS, grid=True)
+    ax.set(xlabel=XLABEL, ylabel="Agents Count")
     ax.set_title(title, size=16, fontweight="bold")
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def capitalization_plot(df, title='cap in eth and cap per agent', figsize=_figsize):
-    ax1 = df.plot(y='capitalization_per_agent', figsize=figsize, xticks=_xticks, grid=True, style={'capitalization_per_agent': 'r'}, logy=True)
-    ax1.set(xlabel=_xlabel, ylabel="eth")
+def capitalization_plot(df: pd.DataFrame, title: str = 'BOOT Capitalization and BOOT Capitalization per Agent, ETH',
+                        figsize: tuple = FIGSIZE):
+    ax1 = df.plot(y='capitalization_per_agent', figsize=figsize, xticks=XTICKS, grid=True, 
+                  style={'capitalization_per_agent': 'r'}, logy=True)
+    ax1.set(xlabel=XLABEL, ylabel="BOOT Capitalization per Agent, ETH")
     ax1.set_title(title, size=16, fontweight="bold")
     ax2 = ax1.twinx()
     ax2.spines['right'].set_position(('axes', 1.0))
-    ax2.set(ylabel="eth")
-    df.plot.line(ax=ax2, y='capitalization_in_eth', figsize=figsize, xticks=_xticks, grid=True)
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    ax2.set(ylabel="BOOT Capitalization, ETH")
+    df.plot.line(ax=ax2, y='capitalization_in_eth', figsize=figsize, xticks=XTICKS, grid=True)
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def gboot_price_plot(df, title='gboot price and validators revenue', figsize=_figsize):
-    ax1 = df.plot(y='gboot_price', figsize=figsize, xticks=_xticks, grid=True, style={'gboot_price': 'r'})
-    ax1.set(xlabel=_xlabel, ylabel="eth")
+def gboot_price_plot(df: pd.DataFrame, title: str = 'GBOOT Price and Validators Revenue', figsize: tuple = FIGSIZE):
+    ax1 = df.plot(y='gboot_price', figsize=figsize, xticks=XTICKS, grid=True, style={'gboot_price': 'r'})
+    ax1.set(xlabel=XLABEL, ylabel="GBOOT Price, ETH")
     ax1.set_title(title, size=16, fontweight="bold")
     ax2 = ax1.twinx()
     ax2.spines['right'].set_position(('axes', 1.0))
-    ax2.set(ylabel="gboot")
-    df.plot.line(ax=ax2, y='validator_revenue_gboot', figsize=figsize, xticks=_xticks, grid=True, )
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    ax2.set(ylabel="Validators Revenue, GBOOT")
+    df.plot.line(ax=ax2, y='validator_revenue_gboot', figsize=figsize, xticks=XTICKS, grid=True, )
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def cyberlinks_per_day_plot(df, title='cyberlinks per day', figsize=_figsize):
-    ax1 = df.plot(y=['cyberlinks_per_day', 'volt_supply', 'volt_liquid_supply'], figsize=figsize, xticks=_xticks,
+def cyberlinks_per_day_plot(df: pd.DataFrame, title: str = 'cyberLinks per day', figsize: tuple = FIGSIZE):
+    ax1 = df.plot(y=['cyberlinks_per_day', 'volt_supply', 'volt_liquid_supply'], figsize=figsize, xticks=XTICKS,
                   style={'cyberlinks_per_day': 'r'}, grid=True)
-    ax1.set(xlabel=_xlabel, ylabel="cyberlinks | volts")
+    ax1.set(xlabel=XLABEL, ylabel="cyberLinks per day | VOLT")
     ax1.set_title(title, size=16, fontweight="bold")
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def cyberlinks_count_plot(df, title='cyberlinks count', figsize=_figsize):
-    ax1 = df.plot(y=['cyberlinks_count'], figsize=figsize, xticks=_xticks, grid=True)
+def cyberlinks_count_plot(df: pd.DataFrame, title: str = 'cyberLinks Count', figsize: tuple = FIGSIZE):
+    ax1 = df.plot(y=['cyberlinks_count'], figsize=figsize, xticks=XTICKS, grid=True)
     ax1.set_title(title, size=16, fontweight="bold")
-    # ax1 = df.plot.line(x='timestep', y='cyberlinks_count', grid=True, xticks=range(0, 3650, 365), figsize=figsize, title=title)
-    # ax2 = ax1.twinx()
-    # ax2.spines['right'].set_position(('axes', 1.0))
-    # df.plot.line(ax=ax2, y=['ampere_supply', 'ampere_liquid_supply'], figsize=figsize, xticks=range(0, 3650, 365),
-    #                   grid=True)
-    # # plt.legend(bbox_to_anchor=(1.0, 1.0))
-    ax1.set(xlabel=_xlabel, ylabel="cyberlinks")
+    ax1.set(xlabel=XLABEL, ylabel="cyberLinks Count")
     ax1.set_title(title, size=16, fontweight="bold")
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def volt_and_ampere_supply_plot(df, title='volt and ampere supply', figsize=_figsize):
+def volt_and_ampere_supply_plot(df: pd.DataFrame, title: str = 'AMPERE and VOLT Supply', figsize: tuple = FIGSIZE):
     columns = ['volt_supply', 'ampere_supply']
-    ax1 = df.plot.area(y=columns, linewidth=0, colormap="winter", xticks=_xticks, grid=True, figsize=figsize)
-    ax1.set(xlabel=_xlabel, ylabel="tokens")
+    ax1 = df.plot.area(y=columns, linewidth=0, colormap="winter", xticks=XTICKS, grid=True, figsize=figsize)
+    ax1.set(xlabel=XLABEL, ylabel="AMPERE | VOLT")
     ax1.set_title(title, size=16, fontweight="bold")
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def mint_rate_plot(df, title='ampere and volt mint rate investmint max', figsize=_figsize):
-    ax1 = df.plot(y=['ampere_mint_rate', 'volt_mint_rate'], figsize=figsize, xticks=_xticks, grid=True)
-    ax1.set(xlabel=_xlabel, ylabel="share")
+def mint_rate_plot(df: pd.DataFrame, title: str = 'AMPERE and VOLT Mint Rate Investmint Max', figsize: tuple = FIGSIZE):
+    ax1 = df.plot(y=['ampere_mint_rate', 'volt_mint_rate'], figsize=figsize, xticks=XTICKS, grid=True)
+    ax1.set(xlabel=XLABEL, ylabel="Share")
     ax1.set_title(title, size=16, fontweight="bold")
     ax2 = ax1.twinx()
-    ax2.set(ylabel="days")
+    ax2.set(ylabel="Days")
     ax2.spines['right'].set_position(('axes', 1.0))
-    df.plot.line(ax=ax2, y='investmint_max_period', figsize=figsize, xticks=_xticks,
-                      style={'investmint_max_period': 'r'}, grid=True)
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    df.plot.line(ax=ax2, y='investmint_max_period', figsize=figsize, xticks=XTICKS,
+                 style={'investmint_max_period': 'r'}, grid=True)
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def minted_volt_ampere_plot(df, title='ampere and volt mint rate investmint max', figsize=_figsize):
+def minted_volt_ampere_plot(df: pd.DataFrame, title: str = 'AMPERE and VOLT Minted Amount', 
+                            figsize: tuple = FIGSIZE):
     columns = ['volt_minted_amount', 'ampere_minted_amount']
-    ax1 = df.plot.line(x='timestep', y=columns, xticks=_xticks, grid=True, figsize=figsize)
-    ax1.set(xlabel=_xlabel, ylabel="tokens")
+    ax1 = df.plot.line(x='timestep', y=columns, xticks=XTICKS, grid=True, figsize=figsize)
+    ax1.set(xlabel=XLABEL, ylabel="AMPERE | VOLT")
     ax1.set_title(title, size=16, fontweight="bold")
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
 
 
-def gpu_memory_usage_plot(df, title='gpu memory usage', figsize=_figsize):
-    ax1 = df.plot(y=['cyberlinks_count'], figsize=figsize, xticks=_xticks, grid=True, logy=True)
-    ax1.set(xlabel=_xlabel, ylabel="cyberlinks")
+def gpu_memory_usage_plot(df: pd.DataFrame, title: str = 'GPU Memory Usage', figsize: tuple = FIGSIZE):
+    ax1 = df.plot(y=['cyberlinks_count'], figsize=figsize, xticks=XTICKS, grid=True, logy=True)
+    ax1.set(xlabel=XLABEL, ylabel="cyberLinks Count")
     ax1.set_title(title, size=16, fontweight="bold")
     ax2 = ax1.twinx()
     ax2.spines['right'].set_position(('axes', 1.0))
-    ax2.set(ylabel="bytes")
-    df.plot.line(ax=ax2, y='gpu_memory_usage', figsize=figsize, xticks=_xticks,
-                      style={'gpu_memory_usage': 'r'}, grid=True)
+    ax2.set(ylabel="GPU Memory Usage, Bytes")
+    df.plot.line(ax=ax2, y='gpu_memory_usage', figsize=figsize, xticks=XTICKS,
+                 style={'gpu_memory_usage': 'r'}, grid=True)
     plt.legend(bbox_to_anchor=(1.0, 1.0))
-    plt.savefig(path + title.replace(' ', '_') + '.png')
+    plt.savefig(IMAGES_PATH + title.replace(' ', '_').lower() + '.png')
     plt.show()
